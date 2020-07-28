5d4019fb3f9c11f5016b1cda23dbeb8a8a3dae48
nikdev <nikdev1380@gmail.com>
2020-07-28 18:56:40 +1000

    make databade project

---
+++ app/Http/Controllers/TicketsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tickets;
+use Illuminate\Http\Request;
+
+class TicketsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Tickets  $tickets
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Tickets  $tickets
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Tickets  $tickets
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tickets $tickets)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Tickets  $tickets
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tickets $tickets)
+    {
+        //
+    }
+}
